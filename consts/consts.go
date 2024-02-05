bab3014f46c3163009434bb6607c213de70807fb
shashu009 <147946178+shashu009@users.noreply.github.com>
2024-02-05 17:24:04 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Metacrafters"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "shashank "
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "SR"
 )
 
 var ID ids.ID
